c73bea9314eb60a1f1d49a31f6504f3446b02bf0
defonsen <anshuman_y@ch.iitr.ac.in>
2025-03-25 22:54:18 +0530

    basic work

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/0x0Glitch/hotel-reservation/api"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main(){
+	listenAddr := flag.String("listenAddr",":5001","The listen address of the API server")
+	flag.Parse()
+
+	app := fiber.New()
+
+	apiv1 := app.Group("/api/v1")	
+	apiv1.Get("/user", api.HandleGetUsers)
+
+	apiv1.Get("/user/:id",api.HandleGetUser)
+
+	app.Listen(*listenAddr)
+}
+
+
